9f9642dd8f176204e76d4e932aeac8bd643843d2
LordiAnders <LordiAnders@users.noreply.github.com>
2023-10-17 06:03:23 +0200

    Added bullet_objects to several parts missing them (again)

--- lua/weaponfactorytweakdata.lua
+++ lua/weaponfactorytweakdata.lua
@@ -1,8 +1,38 @@
---Akimbo Bronco .44 (pd2_dlc_osa) was missing animation tweakdata that caused reload animations to not work
+Hooks:PostHook(WeaponFactoryTweakData,"init","VRTweakDataFixes_FactoryTweakFixes",function(self)
+	--Fix Akimbo Bronco .44 (pd2_dlc_osa) reload animations missing from parts
+	self.wpn_fps_pis_x_rage.override.wpn_fps_pis_rage_body_standard.animations.reload = "reload"
+	self.wpn_fps_pis_x_rage.override.wpn_fps_pis_rage_body_smooth.animations.reload = "reload"
 
-Hooks:PostHook(WeaponFactoryTweakData,"_init_x_rage","VRTweakDataFixes_X_Rage_AnimationFix",function(self)
-	if _G.IS_VR then
-		self.wpn_fps_pis_x_rage.override.wpn_fps_pis_rage_body_standard.animations.reload = "reload"
-		self.wpn_fps_pis_x_rage.override.wpn_fps_pis_rage_body_smooth.animations.reload = "reload"
-	end
+	--Several parts were missing bullet object definitions despite existing in their models
+	--Technically not VR specific fixes, these could be noticed in desktop as well
+	self.parts.wpn_fps_m4_upg_m_quick.bullet_objects = {
+		amount = 2,
+		prefix = "g_bullet_"
+	}
+	self.parts.wpn_fps_upg_ak_m_quick.bullet_objects = {
+		amount = 1,
+		prefix = "g_bullet_"
+	}
+	self.parts.wpn_fps_ass_g36_m_quick.bullet_objects = {
+		amount = 28,
+		prefix = "g_bullet_"
+	}
+	self.parts.wpn_fps_smg_mac10_m_quick.bullet_objects = {
+		amount = 1,
+		prefix = "g_bullet_"
+	}
+	self.parts.wpn_fps_smg_sr2_m_quick.bullet_objects = {
+		amount = 3,
+		prefix = "g_bullet_"
+	}
+
+	self.parts.wpn_fps_lmg_par_m_standard.bullet_objects = {
+		amount = 5,
+		prefix = "g_bullet_"
+	}
+
+	self.parts.wpn_fps_sho_rota_m_standard.bullet_objects = {
+		amount = 6,
+		prefix = "g_slug_"
+	}
 end)
