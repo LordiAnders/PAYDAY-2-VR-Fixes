a157e3f3c7f61a96b753c7594436fc1a8a9efd67
LordiAnders <LordiAnders@users.noreply.github.com>
2023-11-21 18:38:10 +0100

    Fixed broken belt resizing
    set_back_button_enabled was resetting the player hand states for seemingly no reason when entering and exiting grids, causing inputs to break on one of the hands, completely breaking the belt resizing function, since you need both hand inputs to grip a grid.

---
+++ lua/vrcustomizationgui.lua
@@ -0,0 +1,22 @@
+function VRBeltCustomization:set_back_button_enabled(enabled)
+	if self._back_button_enabled == enabled then
+		return
+	end
+
+	tag_print("VRBeltCustomization:set_back_button_enabled", hand_id, enabled)
+
+	--[[local player = managers.menu:player()
+
+	if not enabled then
+		player._hand_state_machine:enter_hand_state(hand_id, hand_id == player:primary_hand_index() and "customization" or "customization_empty")
+	else
+		player._hand_state_machine:enter_hand_state(hand_id, hand_id == player:primary_hand_index() and "laser" or "empty")
+	end]] --Breaks belt resizing. It is already handled by backing out of the "INTERFACE" menu where it's safer
+
+	if managers.menu:active_menu() then
+		managers.menu:active_menu().input:focus(false)
+		managers.menu:active_menu().input:focus(true)
+	end
+
+	self._back_button_enabled = enabled
+end
