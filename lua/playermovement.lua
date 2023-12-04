05029d4cc9d8ac00508f281df8e5f638ae49af88
LordiAnders <7148159+LordiAnders@users.noreply.github.com>
2023-12-04 19:48:08 +0100

    Added missing file for teleport fix

---
+++ lua/playermovement.lua
@@ -0,0 +1,3 @@
+Hooks:PreHook(PlayerMovement,"set_ghost_position","VRFixes_set_ghost_position",function(self,pos,unit_position)
+	self._m_pos:set_position(unit_position and unit_position or pos)
+end)
