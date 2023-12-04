a37764595f973af8cf5fe80fe9f7d3ff47256257
LordiAnders <7148159+LordiAnders@users.noreply.github.com>
2023-12-04 19:55:11 +0100

    Fix haphazard correction for teleport fix

--- lua/playermovement.lua
+++ lua/playermovement.lua
@@ -1,3 +1,3 @@
 Hooks:PreHook(PlayerMovement,"set_ghost_position","VRFixes_set_ghost_position",function(self,pos,unit_position)
-	self._m_pos:set_position(unit_position and unit_position or pos)
+	self._m_pos = unit_position and unit_position or pos
 end)
