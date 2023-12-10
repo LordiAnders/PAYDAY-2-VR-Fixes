ce04f09a1a243c3ab649e2cbb2fd11e448a032de
LordiAnders <7148159+LordiAnders@users.noreply.github.com>
2023-12-10 19:24:01 +0100

    Add comment to playermovement.lua, describing what it does

--- lua/playermovement.lua
+++ lua/playermovement.lua
@@ -1,3 +1,4 @@
+--Fixes the Collision Teleport setting causing actual teleports to fail (Border Crossing, out of bounds, ziplines, etc)
 Hooks:PreHook(PlayerMovement,"warp_to","VRFixes_warp_to",function(self,pos)
 	mvector3.set(self._m_pos,pos)
 end)
